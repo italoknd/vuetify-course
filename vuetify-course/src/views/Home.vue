4192bfe003690db40e2663ac4eb096f9b59b7910
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-03-15 01:45:08 -0400

    trabalhando com breakpoints e visibility

--- vuetify-course/src/views/Home.vue
+++ vuetify-course/src/views/Home.vue
@@ -4,6 +4,10 @@
     <p class="purple white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet reiciendis cupiditate culpa quaerat ullam veritatis beatae velit sequi, repellat aperiam voluptates dicta tenetur at saepe quis quasi maiores cumque similique.</p>
     
     <h1 class="display-4">Massive title</h1>
+
+    <!-- <v-btn color="hidden-lg-and-up">Click Me</v-btn> -->
+    <!-- <v-btn color="hidden-lg-and-down">Click Me</v-btn> -->
+    <!-- <v-btn color="hidden-xs-only">Click Me</v-btn> -->
   </div>
 </template>
 
